da513519409c396bcc6551ff3f42f99107ff27d7
talgat <15talgat@mail.ru>
2022-12-01 17:47:23 +0300

    done zadacha_1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.Clear();
+string EnterNumber(string str)
+{
+    Console.Write(str);
+    string number = Console.ReadLine();
+    return number;
+}
+
+string ReverseString(string str)
+    {
+    char[] array = str.ToCharArray();
+    Array.Reverse(array);
+    return new string(array);
+    }
+
+
+
+string number = EnterNumber("Введите пятизначное число: ");
+string reverse = ReverseString(number);
+if (number == reverse)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
+
