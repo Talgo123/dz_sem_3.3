1f9d45a51080bb6f48f62f8f616809c111e6db3a
talgat <15talgat@mail.ru>
2022-12-01 18:13:30 +0300

    done zadacha_3

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int EnterNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+void FindCubeNumber(int number)
+{
+    int count = 1;
+    while (count < number + 1)
+    {
+        double result = Math.Pow(count, 3);
+        Console.Write($"{result} ");
+        count++;
+    }
+}
+
+int digit = EnterNumber("Введите цифру: ");
+FindCubeNumber(digit);
